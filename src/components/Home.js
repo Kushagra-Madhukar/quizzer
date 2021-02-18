949c258a84f8c103ecc66ace5b50d784af53977a
Kushagra-Madhukar <madhukarkushagra@gmail.com>
2021-02-19 04:27:37 +0530

    Image API request Logic, Game logic complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,58 +1,87 @@
-import axios, {} from 'axios'
-import React, {useState, useEffect} from 'react'
-import { useHistory } from 'react-router-dom'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+const defaultImage = 'https://www.indianlink.com.au/wp-content/uploads/2020/06/Leeds-round-of-Great-Legal-Quiz-set-for-20-November-800x500_c.png'
 
 const Home = () => {
-    const history = useHistory()
-    const [categories, setCategories] = useState()
-    const [gotdata, setGotdata] = useState(false)
-    const [images, setImages] = useState([])
-    useEffect(() => {
-        let mounted = false
-        if(!mounted){
-            axios.get('https://opentdb.com/api_category.php')
-                .then(res => setCategories(res.data.trivia_categories))
-                .then(() => setGotdata(true))
-        }
-        return () => {
-            mounted = true
-        }
-    }, [])
+  const history = useHistory();
+  const [categories, setCategories] = useState();
+  const [gotdata, setGotdata] = useState(false);
+  const [images, setImages] = useState({});
+  useEffect(() => {
+    let mounted = false;
+    if (!mounted) {
+      axios
+        .get("https://opentdb.com/api_category.php")
+        .then((res) => setCategories(res.data.trivia_categories))
+        .then(() => setGotdata(true));
+    }
+    return () => {
+      mounted = true;
+    };
+  }, []);
 
-    // useEffect(() => {
-    //     let mounted = false
-    //     let arre = []
-    //     if(!mounted && categories !== undefined){
-    //             categories.map(categ => {
-    //                 axios.get(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q='${categ.name}'`)
-    //                     .then(res => {setImages([...images, {id: categ.id, url: res.data.totalHits !== 0 ? res.data.hits[0].pageURL : null}])})
-    //                     .then(() => console.log(images))
-    //                 });
-            
-    //     }
-    //     return () => {
-    //         mounted = true
-    //     }
-    // }, [gotdata])
-
-    useEffect(() => {
-        axios.get('https://opentdb.com/api_category.php')
-            .then(res => console.log(res.data.trivia_categories))
-        return () => {
+  const [idPtr, setIdPtr] = useState(0)
+  useEffect(() => {
+    let mounted = false;
+    if (!mounted && categories !== undefined && idPtr < categories.length) {
         
-        }
-    }, [])
 
-    return (
-        <div>
-            Quiz
-            <div>
-                {categories !== undefined ?
-                categories.map(categ => 
-                    <div style={{display: 'flex'}} onClick={() => history.push(`/${categ.id}`)} >{categ.name}</div>) : null}
-            </div>
-        </div>
-    )
-}
+        axios
+          .get(
+            `https://pixabay.com/api/?key=${
+              process.env.REACT_APP_PIXABAY_API_KEY
+            }&q=${categories[idPtr].name.replace(/[^a-zA-Z ]/g, "")}`
+          )
+          .then((res) => {
+            setImages({
+              ...images,
+              [categories[idPtr].id]: {url: res.data.totalHits !== 0 ? res.data.hits[Math.floor(Math.random()*res.data.hits.length)].webformatURL : null},
+            });
+          })
+          .then(() => {setIdPtr(idPtr => idPtr+1)});
 
-export default Home
+         
+
+        
+    //   });
+    }
+    return () => {
+      mounted = true;
+    };
+  }, [gotdata, idPtr]);
+
+
+  const checkIfExist = (id) => {
+    let exist = true;
+    if(images[id] === undefined || images[id].url === null)
+    {
+        exist = false
+    }
+    return exist
+  }
+  return (
+    <div>
+      Quiz
+      <div>
+        {categories !== undefined
+          ? categories.map((categ) => (
+              <div
+                style={{ display: "flex" }}
+                onClick={() => history.push(`/${categ.id}`)}
+              >{checkIfExist(categ.id) ?
+                <img src={images[categ.id].url} alt={`${categ.name} Quiz`}/> 
+                :
+                <img src={defaultImage} alt={`${categ.name} Quiz`}/>
+              }  
+                {categ.name}
+              </div>
+            ))
+          : null}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
