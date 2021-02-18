e77785bfb357c8220214c6cceef3efb01eca7a34
Kushagra-Madhukar <madhukarkushagra@gmail.com>
2021-02-18 23:48:17 +0530

    quiz logic, score and timer management

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,28 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
-const Card = ({Q, A, I}) => {
-    const [options, setoptions] = useState([...I, A])
+const Card = ({Q, A, I, setScore}) => {
+    const [options, setOptions] = useState([...I, A])
+    const [selected, setSelected] = useState()
+    const [ansChanged, setAnsChanged] = useState(false)
+    useEffect(() => {
+        if(selected === undefined){
+            // setScore(score => score)
+        }
+        else if(selected === A) setScore(score => score+4)
+        else setScore(score => score-1)
+    }, [ansChanged])
+
+    const selectHandler = (option) => {
+        setSelected(option)
+        setAnsChanged(!ansChanged)
+    }
     return (
         <div style={{display: 'flex',
         width: '100%'}}>
             <div dangerouslySetInnerHTML={{ __html: Q}}></div>
             <form>
                 {options.map(option => 
-                <div><input type='radio'/>{option}</div>
+                <div><input type='radio' name={option} value={option} checked={option === selected} onChange={() => selectHandler(option)}/><div dangerouslySetInnerHTML={{ __html: option}}/></div>
                 )
             }
             </form>
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,13 +1,25 @@
 import axios from 'axios'
 import React, {useState, useEffect} from 'react'
-import { useParams } from 'react-router-dom'
+import { useHistory, useParams } from 'react-router-dom'
 import Card from './Card'
 
+const timeInterval = 1000
+const testTime = 60000
+const number_questions = 10
+const initialScores = {}
+for(let i = 0;i<number_questions;i++){
+    initialScores[i] = 0
+}
+console.log("Init",initialScores)
 const Quiz = () => {
+    const history = useHistory()
     const [difficulty, setDifficulty] = useState('easy')
-    const [questionsLeft, setQuestionsLeft] = useState(10)
+    const [questionsLeft, setQuestionsLeft] = useState(number_questions)
     const [questions, setQuestions] = useState([])
     const [timer, setTimer] = useState(false)
+    const [gameover, setGameover] = useState(false)
+    const [timeLeft, setTimeLeft] = useState(testTime)
+    const [score, setScore] = useState(0)
     const {id} = useParams()
     // useEffect(() => {
     //     axios.get(`https://opentdb.com/api.php?amount=10&category=${id}&difficulty=${difficulty}`)
@@ -16,14 +28,32 @@ const Quiz = () => {
     //     }
     // }, [input])
     // console.log(id)
+   useEffect(() => {
+       let timer2
+       if(timer){
+            timer2 = setTimeout(() => {
+                setTimeLeft(timeLeft => timeLeft-timeInterval)
+            },timeInterval)
+        }
+       return () => {
+           clearTimeout(timer2)
+       }
+   }, [timer, timeLeft])
+
     useEffect(() => {
         let timer1
+        let a
         if(timer){
+            a = 10
             timer1 = setTimeout(() => {
                 setTimer(false)
-            }, 60000);
+                setGameover(true)
+            }, testTime);
+            console.log("a inside", a)
         }
+        console.log("a",a)
         return () => {
+            console.log("a beta", a)
             clearTimeout(timer1)
         }
     }, [timer])
@@ -32,12 +62,20 @@ const Quiz = () => {
         Event.preventDefault()
         // console.log(id)
         // console.log(difficulty)
-        axios.get(`https://opentdb.com/api.php?amount=10&category=${id}&difficulty=${difficulty}`)
+        axios.get(`https://opentdb.com/api.php?amount=${number_questions}&category=${id}&difficulty=${difficulty}`)
             .then(res => {setQuestions(res.data.results); setTimer(true);console.log(res.data.results[0])})
             .catch(err => console.log(err))
     }
-    
-        if(!timer && questionsLeft==10){
+
+    const resetHandler = () => {
+        setQuestionsLeft(number_questions)
+        setQuestions([])
+        setTimer(false)
+        setGameover(false)
+        setTimeLeft(testTime)
+        setScore(0)
+    }
+        if(!timer && !gameover){
             return(   
             <form onSubmit={submitHandler}>
                 <label>Please choose difficulty</label>
@@ -50,12 +88,22 @@ const Quiz = () => {
             </form>
             )
         }
-        else if(timer && questionsLeft>0){
+        else if(timer && !gameover){
             return(
                 <div>
                     {questions.map(q => (<div style={{display: 'flex', flex: 1}}>
-                        <Card Q={q.question} I = {q.incorrect_answers} A = {q.correct_answer}/>
+                        <Card Q={q.question} I = {q.incorrect_answers} A = {q.correct_answer} setScore={setScore}/>
                     </div>))}
+                    <div>{timeLeft/timeInterval}</div>
+                </div>
+            )
+        }
+        else if(gameover){
+            return (
+                <div>
+                <div>Your Scorecard, your score is {score}</div>
+                <button onClick={() => history.push('/')}>Go to home</button>
+                <button onClick={resetHandler}>Retry</button>
                 </div>
             )
         }
